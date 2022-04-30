d9bface5db1d627698a1e068d55d01d266e8d739
Taras Seniv <taras.seniv@crunch.is>
2022-04-30 16:40:46 +0300

    removed service

--- database/index.js
+++ database/index.js
@@ -1,8 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-console.log('----');
-console.log(process.env.DB_HOST);
-
 const sequelize = new Sequelize(
 	process.env.DB_NAME,
 	process.env.DB_USER,
