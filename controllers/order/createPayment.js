8ea181971b62a948e0fa5ba12b695785a22883e9
Taras Seniv <taras.seniv@crunch.is>
2022-04-30 22:22:53 +0300

    added url for payment

--- controllers/order/createPayment.js
+++ controllers/order/createPayment.js
@@ -31,7 +31,7 @@ const createPayment = async (req, res) => {
 	const priceToFixed = totalPrice.toFixed(2);
 
 	const requestData = {
-		server_callback_url: 'https://27e7-93-175-201-145.eu.ngrok.io/orders/payed',
+		server_callback_url: 'http://18.184.79.221:3000/orders/payed',
 		order_id: `#${orderNumber}/${orderId}`,
 		order_desc: `Order #${orderNumber}`,
 		currency: CURRENCY,
